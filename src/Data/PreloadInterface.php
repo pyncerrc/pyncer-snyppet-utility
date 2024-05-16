fa97baa9482ccfa09c61a387cfda931883c270aa
Xlient <root@xlient.com>
2024-05-15 23:37:37 -0400

    Fixed missing semicolon.

--- src/Data/PreloadInterface.php
+++ src/Data/PreloadInterface.php
@@ -6,5 +6,5 @@ interface PreloadInterface
     public function getPreload(string $key): bool;
     public function setPreload(string $key, bool $value): static;
 
-    public function preload(): static
+    public function preload(): static;
 }
